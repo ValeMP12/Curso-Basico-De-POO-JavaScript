e5e7dd6b562ac1b8dfcef74a290b241f1ad7189d
Valeria Preciado <preciadovaleriam@gmail.com>
2023-09-10 17:52:39 -0600

    Objetos literales y prototipos en JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 const valeria = {
     name: "Valeria",
+    age: 20,
+    cursosAprobados: [
+        "Cursos definitivos de HTML y CSS ",
+        "Curso Práctico de HTML y CSS",
+    ],
+
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+};
+
+
+function Student(name, age, cursosAprobados){
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+    //Esta es una forma
+    //this.aprobarCurso = function(nuevoCurso){
+      //  this.cursosAprobados.push(nuevoCurso);
+    //}
 }
+// Se crea este metodo por fuera de la funcion con "prototype"
+Student.prototype.aprobarCurso = function(nuevoCurso){
+    this.cursosAprobados.push(nuevoCurso);
+}
+//Crear una instancia 
+const monserrat = new Student(
+    "Monserrat",
+    22, [
+        "Curso de Introducción a los videojuegos"
+    ]
+
+);
