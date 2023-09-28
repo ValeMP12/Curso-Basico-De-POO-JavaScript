bd5a37d5c6ac9ab9608d6f49a615450b76cb108a
Valeria Preciado <preciadovaleriam@gmail.com>
2023-09-28 17:56:39 -0600

    Clases con objetos

--- main.js
+++ main.js
@@ -1,56 +1,37 @@
-const valeria = {
-    name: "Valeria",
-    age: 20,
-    cursosAprobados: [
-        "Cursos definitivos de HTML y CSS ",
-        "Curso Práctico de HTML y CSS",
-    ],
-
-    aprobarCurso(nuevoCurso){
-        this.cursosAprobados.push(nuevoCurso);
-    }
-};
-
-
-function Student(name, age, cursosAprobados){
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-    //Esta es una forma
-    //this.aprobarCurso = function(nuevoCurso){
-      //  this.cursosAprobados.push(nuevoCurso);
-    //}
-}
-// Se crea este metodo por fuera de la funcion con "prototype"
-Student.prototype.aprobarCurso = function(nuevoCurso){
-    this.cursosAprobados.push(nuevoCurso);
-}
-//Crear una instancia 
-const monserrat = new Student(
-    "Monserrat",
-    22, [
-        "Curso de Introducción a los videojuegos"
-    ]
-
-);
-
-//Prototipo con la sintaxis de clases
-class Student2 {
-    constructor({name, age,cursosAprobados =[], email}){
+class Student {
+    constructor ({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
+    }){
         this.name = name;
         this.email = email;
-        this.age = age;
-        this.cursosAprobados = cursosAprobados;
-
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
     }
-    aprobarCurso(nuevoCurso){
-        this.cursosAprobados.push(nuevoCurso);
-    }
-};
+}
 
-const juanito = new Student2 ({
-    email: "juanito7@gamil.com",
-    name: "Juan",
-    age: 36,
-    //cursosAprobados: ["Curso Análisis de negocios para ciencia de datos","Curso de principios de visualisacion de datos para BI"],
+const monse = new Student ({
+    name: "Monserrat",
+    username: "monseP12",
+    email: "monserrat@gmail.com",
+    twitter: "monseP12"
+});
+
+const joana = new Student ({
+    name: "Joana",
+    username: "joanaP12",
+    email: "joanaPG12@gmail.com",
+    instagram: "JOANAP12"
 });
