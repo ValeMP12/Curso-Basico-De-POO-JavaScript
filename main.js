12864b5345eb33c2b7d79c06562da6eed032cb7c
Valeria Preciado <preciadovaleriam@gmail.com>
2023-09-28 20:12:24 -0600

    Abstracción en JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+class Course {
+    constructor ({
+        name,
+        classes = [],
+    }){
+        this.name = name;
+        this.classes = classes;
+    }
+}
+
+const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programacion Basica",
+});
+const cursoDefinitivoHTML = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+});
+const cursoPracticoHTML = new Course({
+    name: "Curso Practico de HTML y CSS",
+});
+class learningPaths {
+    constructor({
+        name,
+        course = [],
+    }){
+        this.name = name;
+        this.course = course;
+}
+}
+const escuelaWeb = new learningPaths({
+    name: "Escuela de Desarrollo Web",
+    course: [
+        cursoProgBasica,
+        cursoDefinitivoHTML,
+        cursoPracticoHTML,
+    ],
+});
+const escuelaData = new learningPaths({
+    name: "Escuela de Data Science",
+    course: [
+        cursoProgBasica,
+        "Curso DataBusiness",
+        "Curso Dataviz",
+    ],
+});
+const escuelaVgs = new learningPaths({
+    name: "Escuela de Videojuegos",
+    course: [
+        cursoProgBasica,
+        "Curso de Unity",
+        "Curso de Unreal",
+    ],
+});
+
 class Student {
     constructor ({
         name,
