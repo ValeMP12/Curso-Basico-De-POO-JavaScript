8fba207f59bc49a20422dd24e721b375b3f7788d
Valeria Preciado <preciadovaleriam@gmail.com>
2023-11-02 17:16:04 -0600

    Object.defineProperty

--- main.js
+++ main.js
@@ -1,238 +1,48 @@
-class Comment {
-    constructor({
-        content,
-        studentName,
-        studentRole = "estudiante"
-    }){
-        this.content = content;
-        this.studentName = studentName;
-        this.studentRole = studentRole;
-        this.likes = 0;
-    }
-
-    publicar(){
-        console.log(this.studentName + "(" + this.studentRole + ")");
-        console.log(this.likes + "likes");
-        console.log(this.content);
-    }
-}
-
-//Módulos de ECMAScript6
-function videoPlay(id){
-    const urlSecreta = "https: //platziultrasecretomasquelanasa.com/" + id;
-    console.log("Se esta priduciondo desde la url " + urlSecreta);
-}
-function videoStop(id){
-    const urlSecreta = "https: //platziultrasecretomasquelanasa.com/" + id;
-    console.log("Pausamos la url " + urlSecreta);
-}
-
-class PlatziClass {
-    constructor({
-        name,
-        videoID,
-
-
-    }){
-        this.name = name;
-        this.videoID = videoID;
-    }
-
-    reprodicir (){
-        videoPlay(this.videoID);
-    }
-
-    pausar (){
-        videoStop(this.videoID);
-    }
-}
-
-//Getters y Setters
-class Course {
-    constructor ({
-        name,
-        classes = [],
-        isFree = false,
-        lang = "spanish",
-
-    }){
-        this._name = name;
-        this.classes = classes;
-        this.isFree = isFree;
-        this.lang = lang;
-    }
-
-    get name(){
-        return this._name;
-    }
-    set name(nuevoNombre){
-        if (nuevoNombre === "Curso malito de programacion Básica"){
-            console.error("Web... no");
-        }else{
-            this._name = nuevoNombre;
-        }
-        
-    }
-}
-
-const cursoProgBasica = new Course({
-    name: "Curso Gratis de Programacion Basica",
-    isFree: true,
-});
-const cursoDefinitivoHTML = new Course({
-    name: "Curso Definitivo de HTML y CSS",
-});
-const cursoPracticoHTML = new Course({
-    name: "Curso Practico de HTML y CSS",
-    lang: "english",
-});
-class learningPaths {
-    constructor({
-        name,
-        course = [],
-    }){
-        this.name = name;
-        this.course = course;
-}
-}
-const escuelaWeb = new learningPaths({
-    name: "Escuela de Desarrollo Web",
-    course: [
-        cursoProgBasica,
-        cursoDefinitivoHTML,
-        cursoPracticoHTML,
-    ],
-});
-const escuelaData = new learningPaths({
-    name: "Escuela de Data Science",
-    course: [
-        cursoProgBasica,
-        "Curso DataBusiness",
-        "Curso Dataviz",
-    ],
-});
-const escuelaVgs = new learningPaths({
-    name: "Escuela de Videojuegos",
-    course: [
-        cursoProgBasica,
-        "Curso de Unity",
-        "Curso de Unreal",
-    ],
-});
-
-class Student {
-    constructor ({
-        name,
-        email,
-        username,
-        twitter = undefined,
-        instagram = undefined,
-        facebook = undefined,
-        approvedCourses = [],
-        learningPaths = [],
-    }){
-        this.name = name;
-        this.email = email;
-        this.username = username;
-        this.socialMedia = {
-            twitter,
-            instagram,
-            facebook,
-        };
-        this.approvedCourses = approvedCourses;
-        this.learningPaths = learningPaths;
-    }
-    publicarComentario(commentContent){
-        const comment = new Comment({
-            content: commentContent,
-            studentName: this.name,
-
-        });
-        comment.publicar();
-
-    }
-}
-
-class FreeStuden  extends Student{
-    constructor(props){
-        super(props);
-    }
-
-    approvedCourses(newCurse){
-        if (newCurse.isFree){
-            this.approvedCourses.push(newCurse);
-        }else{
-            console.warn("Lo sentimos," + this.name + ", solo puedes tomar cursos abiertos");
-        }
-    }
-}
-
-class BasicStudent extends Student{
-    constructor(props){
-        super(props);
-    }
-
-    approvedCourses(newCurse){
-        if (newCurse.lang !== "english"){
-            this.approvedCourses.push(newCurse);
-        }else{
-            console.warn("Lo sentimos," + this.name + ", no puedes tomar cursos en inglés");
-        }
-    }
-}
-class ExpertStudent extends Student{
-    constructor(props){
-        super(props);
-    }
-
-    approvedCourses(newCurse){
-        this.approvedCourses.push(newCurse);
-    }
-}
-class TeacherStudent extends Student{
-    constructor(props){
-        super(props);
-    }
-    approvedCourses(newCurse){
-        this.approvedCourses.push(newCurse);
-    }
-    publicarComentario(commentContent){
-        const comment = new Comment({
-            content: commentContent,
-            studentName: this.name,
-            studentRole: "profesor",
-        });
-        comment.publicar();
-    }
-}
-
-
-
-const monse = new FreeStuden ({
-    name: "Monserrat",
-    username: "monseP12",
-    email: "monserrat@gmail.com",
-    twitter: "monseP12",
-    learningPaths: [
-        escuelaWeb,
-        escuelaVgs,
-    ],
-});
-
-const joana = new BasicStudent ({
-    name: "Joana",
-    username: "joanaP12",
-    email: "joanaPG12@gmail.com",
-    instagram: "JOANAP12",
-    learningPaths: [
-        escuelaWeb,
-        escuelaVgs,
-    ],
-});
-
-const valeria = new TeacherStudent({
+const valeria = {
     name: "Valeria",
-    username: "valeriaP12",
-    email: "valeriaPG12@gmail.com",
-    instagram: "VALERIAP12", 
-});
+    age: 20,
+    approvedCourses: ["Curso 1"],
+    addCouse(newCourse){
+        console.log("This", this);
+        console.log("This.approvedCourse", this.approvedCourses);
+        this.approvedCourses.push(newCourse);
+    }
+};
+
+//console.log(Object.keys(valeria));
+//console.log(Object.getOwnPropertyNames(valeria));
+//console.log(Object.entries(valeria));
+
+
+
+//Object.defineProperty(valeria, "pruebNASA", {
+  //  value: "extraterrestres",
+   // enumerable: false,
+   // writable: false,
+    //configurable: false,
+//});
+
+//Object.defineProperty(valeria, "navigator", {
+  //  value: "Chrome",
+    //enumerable: false,
+    //writable: true,
+    //configurable: true,
+//});
+
+//Object.defineProperty(valeria, "editor", {
+ //   value: "VSCode",
+  //  enumerable: true,
+  //  writable: false,
+   // configurable: true,
+//});
+
+//Object.defineProperty(valeria, "terminal", {
+  //  value: "WSL",
+   // enumerable: true,
+   // writable: true,
+   // configurable: false,
+//});
+Object.seal(valeria);
+Object.freeze(valeria);
+
+console.log(Object.getOwnPropertyDescriptors(valeria));
