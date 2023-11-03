962365f95ea31d2bb529de043ee1881fb51e9ba5
Valeria Preciado <preciadovaleriam@gmail.com>
2023-11-02 23:37:36 -0600

    Recursividad

--- Curso-Intermedio-POO-Con-Js/main.js
+++ Curso-Intermedio-POO-Con-Js/main.js
@@ -1,12 +1,37 @@
-const obj1 = {
-  a: "a",
-  b: "b",
-  c:{
-    d: "d",
-    e: "e",
-  }
-};
+//const obj1 = {
+  //a: "a",
+  //b: "b",
+  //c:{
+    //d: "d",
+    //e: "e",
+  //}
+//};
 
 //const obj2 = JSON.stringify(obj1);
-const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
+//const stringifiedComplexObj = JSON.stringify(obj1);
+//const obj2 = JSON.parse(stringifiedComplexObj);
+
+//fuction recursiva(){
+ // if (/*validacion*/){
+    //llamados recursivos
+  //}else{
+    // llamados normales, sin recursividad
+  //}
+//}
+
+const numeritos = [0,1,2,3,4,5,6,7,8,4574,3,6,3];
+//let numerito = 0;
+//for (let index = 0; index < numeritos.length; index ++){
+  //numerito = numeritos[index];
+  //console.log({index, numerito});
+//};
+
+// RECURSIVIDAD
+function recursiva(numbersArray){
+  if (numbersArray.length != 0) {
+    const firstNum = numbersArray [0];
+    console.log(firstNum);
+    numbersArray.shift()
+    recursiva(numbersArray);
+  }
+};
