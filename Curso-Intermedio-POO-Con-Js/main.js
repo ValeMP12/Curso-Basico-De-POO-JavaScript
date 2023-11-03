d32ca81e9f5568ffb7ff28c33ec19a3ae05f5e53
Valeria Preciado <preciadovaleriam@gmail.com>
2023-11-02 21:35:37 -0600

    JSON.parse y JSON.stringify

--- Curso-Intermedio-POO-Con-Js/main.js
+++ Curso-Intermedio-POO-Con-Js/main.js
@@ -7,10 +7,6 @@ const obj1 = {
   }
 };
 
-const obj2 = {};
-for (prop in obj1) {
-  obj2[prop] = obj1[prop];
-};
-
-const obj3 = Object.assign({}, obj1);
-//const obj4 = Object.create({},obj1);
+//const obj2 = JSON.stringify(obj1);
+const stringifiedComplexObj = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObj);
