f2d39c0ee27af0cca3fdd5584d0b98d9f88a521c
Valeria Preciado <preciadovaleriam@gmail.com>
2023-11-02 20:48:03 -0600

    Shallow copy en JavaScript

--- Curso-Intermedio-POO-Con-Js/main.js
+++ Curso-Intermedio-POO-Con-Js/main.js
@@ -1,48 +1,16 @@
-const valeria = {
-    name: "Valeria",
-    age: 20,
-    approvedCourses: ["Curso 1"],
-    addCouse(newCourse){
-        console.log("This", this);
-        console.log("This.approvedCourse", this.approvedCourses);
-        this.approvedCourses.push(newCourse);
-    }
+const obj1 = {
+  a: "a",
+  b: "b",
+  c:{
+    d: "d",
+    e: "e",
+  }
 };
 
-//console.log(Object.keys(valeria));
-//console.log(Object.getOwnPropertyNames(valeria));
-//console.log(Object.entries(valeria));
+const obj2 = {};
+for (prop in obj1) {
+  obj2[prop] = obj1[prop];
+};
 
-
-
-//Object.defineProperty(valeria, "pruebNASA", {
-  //  value: "extraterrestres",
-   // enumerable: false,
-   // writable: false,
-    //configurable: false,
-//});
-
-//Object.defineProperty(valeria, "navigator", {
-  //  value: "Chrome",
-    //enumerable: false,
-    //writable: true,
-    //configurable: true,
-//});
-
-//Object.defineProperty(valeria, "editor", {
- //   value: "VSCode",
-  //  enumerable: true,
-  //  writable: false,
-   // configurable: true,
-//});
-
-//Object.defineProperty(valeria, "terminal", {
-  //  value: "WSL",
-   // enumerable: true,
-   // writable: true,
-   // configurable: false,
-//});
-Object.seal(valeria);
-Object.freeze(valeria);
-
-console.log(Object.getOwnPropertyDescriptors(valeria));
+const obj3 = Object.assign({}, obj1);
+//const obj4 = Object.create({},obj1);
